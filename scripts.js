e9a2a2ceb1e726281aed4f8713a7f4d76ab8bf3e
Geetanjali Venna <geetanjali.venna@gmail.com>
2025-04-26 13:55:02 +0000

    Media Player Test

--- scripts.js
+++ scripts.js
@@ -15,3 +15,18 @@ document.getElementById("open-popup-2").addEventListener("click", function (even
 document.getElementById("close-popup-2").addEventListener("click", function () {
     document.getElementById("popup-2").style.display = "none";
 });
+
+
+// Third popup: Media Player Folder
+const openBtn = document.getElementById("open-media-player");
+const popup = document.getElementById("media-player-popup");
+const closeBtn = document.getElementById("close-media-player");
+    
+openBtn.addEventListener("click", function(event) {
+    event.preventDefault();
+    popup.style.display = "block";
+});
+    
+closeBtn.addEventListener("click", function() {
+    popup.style.display = "none";
+});
