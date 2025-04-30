b939b9acb375f637975c3d4d28030016f5e4efda
Geetanjali Venna <geetanjali.venna@gmail.com>
2025-04-30 15:51:05 +0000

    Media Player Forked

--- media-player-app/src/App.js
+++ media-player-app/src/App.js
@@ -1,18 +1,18 @@
 import React from 'react';
 import { MediaPlayer } from 'win95-media-player';
-import './App.css';
 
 const playlist = [
   {
     url: 'https://archive.org/download/CC1301_windows_95/CC1301_windows_95_512kb.mp4',
-    title: 'Windows 95 Intro',
+    title: 'Computer Chronicles - Windows 95'
   }
 ];
 
 function App() {
   return (
-    <div style={{ padding: '40px', maxWidth: '400px', margin: 'auto' }}>
+    <div className="App">
       <MediaPlayer
+        className="player"
         playlist={playlist}
         showVideo
         fullscreenEnabled
@@ -22,3 +22,4 @@ function App() {
 }
 
 export default App;
+
