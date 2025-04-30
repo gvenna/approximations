1a396a0d3342e634fb83de73e2e6c5eb2c117293
Geetanjali Venna <geetanjali.venna@gmail.com>
2025-04-30 05:20:33 +0000

    Media

--- scripts.js
+++ scripts.js
@@ -35,3 +35,27 @@ function updateClock() {
 
 setInterval(updateClock, 1000);
 updateClock(); // Update immediately
+
+
+// JavaScript
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { MediaPlayer } from 'win95-media-player';
+
+const playlist = [
+  {
+    url: 'https://archive.org/download/CC1301_windows_95/CC1301_windows_95_512kb.mp4',
+    title: 'Computer Chronicles - Windows 95'
+  }
+];
+
+ReactDOM.render(
+  <MediaPlayer
+    className="player"
+    playlist={playlist}
+    showVideo
+    fullscreenEnabled
+  />,
+  document.getElementById('app')
+);
